d652d4085d36c44f2e92d5d39b58bf72402e3c37
James Jung <jamesjung079@gmail.com>
2023-05-22 18:58:48 -0700

    created .py file for model

---
+++ model.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
+
+import matplotlib.pyplot as plt
